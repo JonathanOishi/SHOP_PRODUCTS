4edc44bf0459e03d42e1f9fc5e61a81e3dafc0c0
Jonathan Oishi <jonathan_oishi@hotmail.com>
2023-02-21 21:12:51 -0300

    Primeiro Commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const userFavoritesUrl =
+      'https://shorp-coder-default-rtdb.firebaseio.com/userFavorites';
+  static const productBaseUrl =
+      'https://shorp-coder-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shorp-coder-default-rtdb.firebaseio.com/orders';
+  static const webApiKey = 'AIzaSyC8V6oNpdHm0JMTcterqvUAKBdD1gsBEpQ';
+}
